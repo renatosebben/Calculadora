1df217e714da7a597ca969f5c73eb969939b2ae8
Renato <renatosebben@gmail.com>
2020-11-09 09:55:29 -0300

    ajuste

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -80,20 +80,20 @@ export default class Calculator extends Component {
         <Display value={this.state.displayValue} />
         <Button label="AC" click={this.clearMemory()} triple />
         <Button label="/" click={this.setOperation} operation />
-        <Button label="7" click={this.addDigito} />
-        <Button label="8" click={this.addDigito} />
-        <Button label="9" click={this.addDigito} />
+        <Button label="7" click={this.addDigit} />
+        <Button label="8" click={this.addDigit} />
+        <Button label="9" click={this.addDigit} />
         <Button label="*" click={this.setOperation} operation />
-        <Button label="4" click={this.addDigito} />
-        <Button label="5" click={this.addDigito} />
-        <Button label="6" click={this.addDigito} />
+        <Button label="4" click={this.addDigit} />
+        <Button label="5" click={this.addDigit} />
+        <Button label="6" click={this.addDigit} />
         <Button label="-" click={this.setOperation} operation />
-        <Button label="1" click={this.addDigito} />
-        <Button label="2" click={this.addDigito} />
-        <Button label="3" click={this.addDigito} />
+        <Button label="1" click={this.addDigit} />
+        <Button label="2" click={this.addDigit} />
+        <Button label="3" click={this.addDigit} />
         <Button label="+" click={this.setOperation} operation />
-        <Button label="0" click={this.addDigito} double />
-        <Button label="." click={this.addDigito} />
+        <Button label="0" click={this.addDigit} double />
+        <Button label="." click={this.addDigit} />
         <Button label="=" click={this.setOperation} operation />
       </div>
     );
